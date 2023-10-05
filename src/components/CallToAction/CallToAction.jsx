672144a11b7a6f220498b2a481b81399a6d9d4f5
Hariton <khariton.aaa.97@gmail.com>
2023-10-05 19:15:14 +0200

    added framer-motion

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -1,14 +1,48 @@
+import { motion } from "framer-motion";
+
 import classes from './CallToAction.module.scss';
 
-import { Button } from 'components/Button'
+import { MButton } from 'components/Button'
 
+
+const textAnimation = {
+  hidden: {
+    y: -100,
+    opacity: 0
+  },
+  visible: custom => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.2
+    }
+  })
+}
+
+const btnAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+}
+
+// I. Если непонятно, то все объяснения в Компонента Hero, Features, Button
 const CallToAction = () => {
   return (
-    <section className={classes.CallToAction}>
-      <h2>Boost your links today</h2>
-      <Button>Get Started</Button>
-    </section>
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.4, once: false }}
+      className={classes.CallToAction}
+      style={{ overflow: "hidden" }}>
+      <h2 variants={textAnimation}>Boost your links today</h2>
+      <MButton variants={btnAnimation}>Get Started</MButton>
+    </motion.section>
   )
 }
 
-export {CallToAction};
+export { CallToAction };
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,31 +1,106 @@
+import { motion } from "framer-motion";
+import { forwardRef } from "react";
+
+
 import classes from './Features.module.scss';
 
 import { features } from './data';
 
+const textAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0
+  },
+  visible: custom => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.2
+    }
+  })
+}
+
+const featureAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0
+  },
+  visible: custom => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.2
+    }
+  })
+}
+
+// II. Для чего нужен forwardRef и в общем писать ref загляни в компонент Hero - там все объяснение (будет пример с MButton)
+// whileInView="visible" Свойство whileInView в библиотеке Framer Motion позволяет вам определить анимации или стили, которые будут применяться к элементу, пока этот элемент находится во вьюпорте (области видимости окна браузера).
+// viewport - Когда viewport установлено в { amount: 0.2 }, это означает, что элемент будет анимироваться, когда он появится во вьюпорте окна на 20% своей высоты. 
+// viewport={{once: false}} - При скролле обратно к этому элементу анимации снова повторится (true - отлючит анимацию)
+
+/* III. 
+  Для чего мы вставили еще раз 
+          это 
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.2, once: false }}
+
+          в 
+          <div>
+            <MFeatureItem />
+          </div>
+
+          и написали для него motion ????
+  Ответ:
+    Мы так сделали, чтобы <div><MFeatureItem/></div> был независимым компонентом в плане анимации, потому что в <motion.section></motion.section> v s дали vieport amount * 0.2 и при скролла до блока h2 анимация срабатывалась и для <h2></h2>, и для <p></p>, и для <MFeatureItem />, а мы хотим, чтобы блок с <MFeatureItem /> был независимым и только при скролле до них и видимость было 20% именно этого блока срабатывалась анимация, вот поэтому мы и прописали для <div><MFeatureItem /></div> отдельно          
+          это  
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.2, once: false }}
+          и дали ему motion и теперь о выглядит так
+          <motion.div></motion.div>
+
+*/
 const Features = () => {
   return (
-    <section className={classes.Features}>
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.2, once: false }}
+      className={classes.Features}>
       <div className="container">
-          <h2 className={classes.title}>{features.title}</h2>
-          <p className={classes.description}>{features.description}</p>
-          <div className={classes.items}>
-            {features.items.map(item => (
-              <FeatureItem key={item.id} {...item} />
-            ))}
-          </div>
+        <motion.h2 custom={1} variants={textAnimation} className={classes.title}>{features.title}</motion.h2>
+        <motion.p custom={2} variants={textAnimation} className={classes.description}>{features.description}</motion.p>
+
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.2, once: false }}
+          className={classes.items}>
+          {features.items.map((item, index) => (
+            <MFeatureItem custom={index + 3} variants={featureAnimation} key={item.id} {...item} />
+          ))}
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
-const FeatureItem = ({title, body, icon}) => (
-  <article className={classes.item}>
+
+// I. Чтобы не писать return он поставил круглые скобки
+const FeatureItem = forwardRef(({ title, body, icon }, ref) => (
+  <article
+    ref={ref}
+    className={classes.item}>
     <figure>
       <img src={icon} alt={title} />
     </figure>
     <h3>{title}</h3>
     <p>{body}</p>
   </article>
-)
+))
 
-export {Features};
+const MFeatureItem = motion(FeatureItem);
+
+export { Features };
